21be2d4f3ef5b76c4045ff6303bb01c23fa191c2
Nisanech <57728976+Nisanech@users.noreply.github.com>
2022-11-04 18:55:42 -0500

    Se agrega funcionalidad para agregar el valor de la tecla seleccionada en la pantalla

--- js/app.js
+++ js/app.js
@@ -9,3 +9,22 @@ const teclaIgual = document.getElementsByName('data-igual')[0];
 var operacionActual = '';
 var operacionAnterior = '';
 var operacion = undefined;
+
+// Capturar evento: Teclado numérico y operadores
+teclas.forEach( function( tecla ) {
+  tecla.addEventListener('click', function(){
+    agregarTecla(tecla.innerText);
+  });
+});
+
+// Función para agregar el valor de la tecla seleccionada
+function agregarTecla( valor ) {
+  operacionActual = operacionActual.toString() + valor.toString();
+
+  actualizarPantalla();
+}
+
+// Función para actualizar la pantalla
+function actualizarPantalla() {
+  pantalla.textContent = operacionActual;
+}
