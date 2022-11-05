fbddf6c934c2a1aa91266cdc56c8540af2f77595
Nisanech <57728976+Nisanech@users.noreply.github.com>
2022-11-05 10:49:04 -0500

    Se agrega evento para cambiar el color de la pantalla en el modo oscuro

--- js/tema.js
+++ js/tema.js
@@ -3,9 +3,12 @@ const btnTema = document.querySelector( '#btn-tema' );
 
 // Variables para los elementos que tendrán el tema oscuro
 const header = document.querySelector( '#header' );
+const display = document.querySelector( '.display' );
+
 
 // Capturar evento: Agregar clases del modo oscuro al presionar el botón
 btnTema.addEventListener( 'click', function() {
   document.body.classList.toggle( 'modo-drk' );
-  header.classList.toggle("header-drk");
+  header.classList.toggle( 'header-drk' );
+  display.classList.toggle( 'pantalla-drk' );
 });
