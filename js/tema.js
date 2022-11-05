93e11b0474e7458bebe8a07b22a1298824d1c93a
Nisanech <57728976+Nisanech@users.noreply.github.com>
2022-11-05 10:50:44 -0500

    Se agrega evento para cambiar el color del teclado en el modo oscuro

--- js/tema.js
+++ js/tema.js
@@ -4,6 +4,7 @@ const btnTema = document.querySelector( '#btn-tema' );
 // Variables para los elementos que tendrán el tema oscuro
 const header = document.querySelector( '#header' );
 const display = document.querySelector( '.display' );
+const teclado = document.querySelector( '#teclado' );
 
 
 // Capturar evento: Agregar clases del modo oscuro al presionar el botón
@@ -11,4 +12,5 @@ btnTema.addEventListener( 'click', function() {
   document.body.classList.toggle( 'modo-drk' );
   header.classList.toggle( 'header-drk' );
   display.classList.toggle( 'pantalla-drk' );
+  teclado.classList.toggle( 'teclado-drk' );
 });
